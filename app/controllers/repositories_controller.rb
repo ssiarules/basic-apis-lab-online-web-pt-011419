bdffe337e8f53bad84f1969fa956992a9ae7f558
Johnson Joseph <jhonsonj88@gmail.com>
2019-09-02 22:35:28 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,11 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+      Faraday.get
+      'https://api.github.com/search/repositories' do |req|
+    req.params['client_id'] = 'client_id'
+    req.params['client_secret'] = 'client_secret'
   end
+  render 'search'
+end
 end
